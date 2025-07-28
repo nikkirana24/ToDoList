70efc7e1f7ee3192019b3222a2436909136a37de
Nikki <nikkiranabhat@gmail.com>
2025-07-28 13:50:24 -0400

    imported swift data

--- ToDoList/ContentView.swift
+++ ToDoList/ContentView.swift
@@ -6,9 +6,13 @@
 //
 
 import SwiftUI
+import SwiftData
 
 struct ContentView: View {
+    
     @State private var showNewTask = false
+    @Query var toDos: [ToDoItem]
+    
     var body: some View {
         VStack {
             HStack {
@@ -30,6 +34,11 @@ struct ContentView: View {
             }
             .padding()
             Spacer()
+            List {
+                ForEach(toDos) { toDoItem in
+                    Text(toDoItem.title)
+                }
+            }
         }
         if showNewTask {
             NewToDoView()
