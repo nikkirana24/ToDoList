8b2d450f3e8528d76890942b97833e075c25fa5b
Nikki <nikkiranabhat@gmail.com>
2025-07-28 13:08:00 -0400

    made a mistake, uploaded the wrong file. this is the correct file now

--- ToDoList/NewToDoView.swift
+++ ToDoList/NewToDoView.swift
@@ -5,4 +5,14 @@
 //  Created by Nikki  on 7/28/25.
 //
 
-import Foundation
+import SwiftUI
+
+struct NewToDoView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    NewToDoView()
+}
