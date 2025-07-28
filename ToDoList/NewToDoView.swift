afd6ac0457050a7ca45179a4b9d1f91eef058f62
Nikki <nikkiranabhat@gmail.com>
2025-07-28 13:21:59 -0400

    added more text to this new view, and added text fields, toggle, and a save button

--- ToDoList/NewToDoView.swift
+++ ToDoList/NewToDoView.swift
@@ -9,7 +9,23 @@ import SwiftUI
 
 struct NewToDoView: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack {
+            Text("Task title:")
+                .font(.system(size:20))
+                .foregroundColor(Color(hue: 0.735, saturation: 0.994, brightness: 0.399))
+            TextField("Enter the task description...", text: /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Value@*/.constant("")/*@END_MENU_TOKEN@*/)
+                .background(Color(.systemGroupedBackground))
+                .cornerRadius(8)
+                .padding()
+            Toggle(isOn: /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Is On@*/.constant(true)/*@END_MENU_TOKEN@*/) {
+                Text("Is it important?")
+                    .padding()
+            }
+            Button {
+            } label: {
+                Text("Save")
+            }
+        }
     }
 }
 
